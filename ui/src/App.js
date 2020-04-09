dba8b6fb408bd8e488d70023e1f8d981d4c96885
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-08 21:35:00 -0400

    Add todo form

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,7 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+              <form action="" id="form">
+                <div className="flex-wrapper">
+                    <div style={{flex:6}}>
+                      <input type="text" className="form-control" id="title" name="title" placeholder="Add a Task"/>
+                    </div>
+                    <div style={{flex:1}}>
+                      <input type="submit" className="btn btn-warning" id="submit" name="title" placeholder="Add" />
+                    </div>
+                </div>
+              </form>
+          </div>
+          <div id="list-wrapper">
 
+          </div>
+        </div>
     </div>
   );
 }
