bcb772444afad973fe8f34f4dcac7f63fe9cf386
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-08 21:10:28 -0400

    Add a route to update a task by id

--- api/urls.py
+++ api/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
     path('task-list/', views.taskList, name="task-list"),
     path('task-detail/<str:id>', views.taskDetail, name="task-detail"),
-    path('task-create/', views.taskCreate, name="task-create")
+    path('task-create/', views.taskCreate, name="task-create"),
+    path('task-update/<str:id>', views.taskUpdate, name="task-update")
 ]
