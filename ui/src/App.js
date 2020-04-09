0a1a5471138dc43768bdd96878fb0b840e835eba
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-08 21:56:54 -0400

    Display all tasks

--- ui/src/App.js
+++ ui/src/App.js
@@ -10,7 +10,8 @@ function App() {
     console.log('Fetching...');
     fetch('/api/task-list/')
     .then(response => response.json())
-    .then(data=> console.log(data));
+    .then(data=> setTodoList(data))
+    .catch(err=> console.log(err));
   };
   
   useEffect(()=>{
@@ -33,7 +34,33 @@ function App() {
               </form>
           </div>
           <div id="list-wrapper">
+          {todoList.map(function(task, index){
+                      return(
+                          <div key={index} className="task-wrapper flex-wrapper">
 
+                            <div onClick={() =>{}} style={{flex:7}}>
+
+                                {task.completed == false ? (
+                                    <span>{task.title}</span>
+
+                                  ) : (
+
+                                    <strike>{task.title}</strike>
+                                  )}
+  
+                            </div>
+
+                            <div style={{flex:1}}>
+                                <button onClick={() => {}} className="btn btn-sm btn-outline-info">Edit</button>
+                            </div>
+
+                            <div style={{flex:1}}>
+                                <button onClick={() => {}} className="btn btn-sm btn-outline-dark delete">-</button>
+                            </div>
+
+                          </div>
+                        )
+                    })}
           </div>
         </div>
     </div>
