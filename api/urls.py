963554c558ff08f70443b91faa691858e76b4b2e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-08 21:14:01 -0400

    Add route to delete a task by id

--- api/urls.py
+++ api/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('task-list/', views.taskList, name="task-list"),
     path('task-detail/<str:id>', views.taskDetail, name="task-detail"),
     path('task-create/', views.taskCreate, name="task-create"),
-    path('task-update/<str:id>', views.taskUpdate, name="task-update")
+    path('task-update/<str:id>', views.taskUpdate, name="task-update"),
+    path('task-delete/<str:id>', views.taskDelete, name="task-delete")
 ]
