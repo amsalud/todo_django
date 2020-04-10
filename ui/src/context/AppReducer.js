ae1f259bf7afe5617c06d38050e49bf295185d9e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-09 21:22:55 -0400

    Handle EDIT_TODO_ITEM action

--- ui/src/context/AppReducer.js
+++ ui/src/context/AppReducer.js
@@ -16,6 +16,14 @@ export default (state, action) => {
                 state,
                 todoList : state.todoList.filter((item)=> item.id !== action.payload)
             }
+        case 'EDIT_TODO_ITEM':
+            const index = state.todoList.findIndex(item=> item.id === action.payload.id);
+            const updatedTodoList = [...state.todoList];
+            updatedTodoList[index] = action.payload;
+            return{
+                state,
+                todoList: [...updatedTodoList]
+            }
         default:
             return state;
     }
