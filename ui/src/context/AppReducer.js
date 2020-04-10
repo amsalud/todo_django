38f903e7bd314489a2061d4b1209485b7a6058e6
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-09 20:08:40 -0400

    Add SET_TODO_LIST action method

--- ui/src/context/AppReducer.js
+++ ui/src/context/AppReducer.js
@@ -1,5 +1,11 @@
 export default (state, action) => {
     switch(action.type){
+
+        case 'SET_TODO_LIST':
+            return {
+                ...state,
+                todoList: action.payload
+            }
         default:
             return state;
     }
